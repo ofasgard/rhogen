0b7c6a5d433f620e77735dd7aa75317081a14be0
Callum Murphy <callum.a.murphy.77@gmail.com>
2022-07-19 21:35:00 +0100

    add additional rounding in markdown report

--- markdown.py
+++ markdown.py
@@ -32,8 +32,9 @@ def export_planet(planet):
 	distance_au = round(planet.distance, 2)
 	radius_km = int(round(util.planetary_radius_to_km(planet.radius), 0))
 	mass_kg = util.planetary_mass_to_kg(planet.mass)
+	mass_rounded = '{:0.3e}'.format(mass_kg)
 	gravity = round(planet.gravity, 2)
 	year_length = round(planet.year_length, 1)
 	temperature_c = round(util.kelvin_to_celsius(planet.temperature), 1)
-	return planet_template.substitute(name=planet.name, distance=distance_au, radius=radius_km, mass=mass_kg, gravity=gravity, year=year_length, temperature=temperature_c, atmosphere=planet.atmosphere)
+	return planet_template.substitute(name=planet.name, distance=distance_au, radius=radius_km, mass=mass_rounded, gravity=gravity, year=year_length, temperature=temperature_c, atmosphere=planet.atmosphere)
 
